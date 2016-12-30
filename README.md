64161a07328a92e0f3cde4dca8199b03a410f9ce
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2016-12-30 15:29:07 +0800

    first commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# xml-springmvc-base_project
+
+This is a stub for demo any xml based spring application.. check it out & add any integration on the branches..
---
+++ src/main/java/personal/arc/mvc/controller/HelloController.java
@@ -0,0 +1,24 @@
+package personal.arc.mvc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import personal.arc.mvc.bean.Person;
+
+/**
+ * Created by Arc on 18/8/2016.
+ */
+@RestController
+@RequestMapping("say")
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String sayHello() {
+        return "Hello world!";
+    }
+
+    @RequestMapping(value = "contact")
+    public Person getInfo() {
+        return new Person().setEmail("286999915@qq.com").setName("钱晟龙");
+    }
+
+}
