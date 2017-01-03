d2551d4cdb13dd95c19b7de5efa7a7ef1a6d0b1d
chinese name : 钱晟龙 english name: Arc Qian <286999915@qq.com>
2017-01-03 10:56:20 +0800

    enable spring session with redis sentinel.

--- src/main/java/personal/arc/mvc/controller/HelloController.java
+++ src/main/java/personal/arc/mvc/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import personal.arc.mvc.bean.Person;
 
+import javax.servlet.http.*;
+
 /**
  * Created by Arc on 18/8/2016.
  */
@@ -12,12 +14,19 @@ import personal.arc.mvc.bean.Person;
 public class HelloController {
 
     @RequestMapping("hello")
-    public String sayHello() {
+    public String sayHello(HttpServletRequest httpServletRequest, HttpServletResponse response) {
+        HttpSession httpSession = httpServletRequest.getSession();
+        String id = httpSession.getId();
+        System.out.println(id);
+        String CURRENT_SESSION_ATTR = HttpServletRequestWrapper.class.getName();
+
+        httpServletRequest.setAttribute(CURRENT_SESSION_ATTR, "可以将current session给换掉, 但必须是从redis拿出来的HttpSessionWrapper(该类型为private)类型的");
+        // HttpSession httpSession2 = httpServletRequest.getSession();
         return "Hello world!";
     }
 
     @RequestMapping(value = "contact")
-    public Person getInfo() {
+    public Person getInfo(HttpServletRequest httpServletRequest, HttpServletResponse response) {
         return new Person().setEmail("286999915@qq.com").setName("钱晟龙");
     }
 
